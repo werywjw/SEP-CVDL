769f068ff74e9cd173604ab3afcb1f06e241edf2
leahkawka <51206985+leahkawka@users.noreply.github.com>
2024-01-27 16:55:41 +0100

    slurm conda test

--- scripts/slurm_job.sh
+++ scripts/slurm_job.sh
@@ -3,9 +3,15 @@
 #SBATCH --output=%x_%j_%N.out
 #SBATCH --error=%x_%j_%N.err
 #SBATCH --partition=Nvidia2060
+#TODO: check how to get files created by script
+
+echo "Starting executio of CVDL_JML"
+echo "$(python3 --version)"
+echo "$(conda --version)"
+# clone repository
+
+# create python environment
 
 # run jupyternotebook headless
 # jupyter nbconvert --to html --execute --ExecutePreprocessor.enabled=False validation_GCAM.ipynb
-echo "Hello World!"
-echo "$(python3 --version)"
 sleep 30

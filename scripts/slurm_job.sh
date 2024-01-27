dad77d9e78a7a391e7f9ac02faa10d1d1f715ab2
leahkawka <51206985+leahkawka@users.noreply.github.com>
2024-01-27 17:02:03 +0100

    slurm conda install

--- scripts/slurm_job.sh
+++ scripts/slurm_job.sh
@@ -6,11 +6,20 @@
 #TODO: check how to get files created by script
 
 echo "Starting executio of CVDL_JML"
-echo "$(python3 --version)"
-echo "$(conda --version)"
+echo $SHELL
 # clone repository
 
 # create python environment
+# install miniconda
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+~/miniconda3/bin/conda init bash
+~/miniconda3/bin/conda init zsh
+
+echo "$(python3 --version)"
+echo "$(conda --version)"
 
 # run jupyternotebook headless
 # jupyter nbconvert --to html --execute --ExecutePreprocessor.enabled=False validation_GCAM.ipynb
